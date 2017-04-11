3971f9b3ff3154b1a777a919e405b4f94569fe8a
phadrian <pheh.adrian@gmail.com>
2017-04-11 22:15:59 +0800

    Implemented api calls to Github API

--- js/scrape.js
+++ js/scrape.js
@@ -1,3 +1,25 @@
-jQuery.githubUser = function(username, callback) {
-    jQuery.getJSON('https://api.github.com/users/' + username + '/repos?callback=?', callback);
+const paToken = 'e7c496d46867a3a0c091cb9fe8fdf71dc73b2b2e';
+const paUser = 'phadrian';
+const endPoint = 'https://api.github.com';
+
+const credentials = paUser + ':' + paToken;
+const auth = btoa(credentials);
+
+const options = {
+    mode: 'cors',
+    headers: {
+        'Authorization': 'Basic ' + auth,
+    }
+}
+
+const api = (path) => {
+    return fetch(endPoint + path, options)
+        .then(
+            response => response.json(),
+            err => console.error('Error fetching', err)
+        )
+        .then(
+            json => console.log('JSON', json),
+            err => console.error('Error parsing', err)
+        );
 }
