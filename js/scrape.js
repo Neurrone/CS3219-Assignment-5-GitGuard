f6785f785e4c49498c939e7973d58fc3a7b9bcf3
phadrian <pheh.adrian@gmail.com>
2017-04-11 23:33:30 +0800

    Added presets for certain API calls

--- js/scrape.js
+++ js/scrape.js
@@ -1,3 +1,7 @@
+// ======================================================================
+// API fetch call
+// ======================================================================
+
 const paToken = 'e7c496d46867a3a0c091cb9fe8fdf71dc73b2b2e';
 const paUser = 'phadrian';
 const endPoint = 'https://api.github.com';
@@ -23,3 +27,37 @@ const api = (path) => {
             err => console.error('Error parsing', err)
         );
 }
+
+// ======================================================================
+// API call function presets
+// ======================================================================
+
+const allCollaboratorsOfRepo = (owner, repo) => {
+    return '/repos/' + owner + '/' + repo + '/collaborators';
+};
+
+const allCommitsOfRepoByUser = (owner, repo, user) => {
+    return '/repos/' + owner + '/' + repo + '/commits?author=' + user;
+};
+
+const allCommitsOfRepoByUserInRange = (owner, repo, user, start, end) => {
+    return '/repos/' + owner + '/' + repo + '/commits?author=' + user + 
+           '&?since=' + start + '&?until=' + end;
+};
+
+// Requires further processing
+// See https://developer.github.com/v3/repos/statistics/
+// Can also be used to check by week
+const allInsDelOfRepo = (owner, repo) => {
+    return '/repos/' + owner + '/' + repo + '/stats/contributors';
+};
+
+const allCommitsOfFile = (owner, repo, filepath) => {
+    return '/repos/' + owner + '/' + repo + '/commits?path=' + filepath;
+};
+
+// Use Javascript Date object to manage subtracting days
+// http://stackoverflow.com/questions/563406/add-days-to-javascript-date
+const allCollaboratorsOfRepoInLastWeek = (owner, repo, lastweek) => {
+    return '/repos/' + owner + '/' + repo + '/collaborators?since=' + lastweek;
+};
