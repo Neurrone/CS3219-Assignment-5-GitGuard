06c5b74cc1b923886f1143d66b63cdcda1124261
Neurrone <neurrone@users.noreply.github.com>
2017-04-13 17:07:41 +0800

    Import github API key from secrets.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import logging
 
 from telegram.ext import CommandHandler, Updater
 
-from secrets import TOKEN
+from secrets import BOT_TOKEN, GIT_USER, GIT_TOKEN
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
@@ -10,7 +10,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
-updater = Updater(token = TOKEN)
+updater = Updater(token = BOT_TOKEN)
 dispatcher = updater.dispatcher
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
