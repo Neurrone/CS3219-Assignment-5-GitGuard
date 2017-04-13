af4f613fa4abf2f5aa76629ac1988ee07624dde0
Terence Kong <kong_terence@hotmail.com>
2017-04-13 19:18:33 +0800

    implement sum_contribution in backend

--- backend/main.py
+++ backend/main.py
@@ -19,5 +19,24 @@ def get_contribution(owner, repo):
     response = api.make_request('repos/{}/{}/stats/contributors'.format(owner, repo))
     return jsonify(response)
 
+@app.route('/<owner>/<repo>/sum_contribution', methods=['GET'])
+def get_sum_contribution(owner, repo):
+    response = api.make_request('repos/{}/{}/stats/contributors'.format(owner, repo))
+    authorList = [];
+
+    for author in response:
+        authorName = author['author']['login'];
+        a = 0;
+        d = 0;
+        c = 0;
+        for week in author['weeks']:
+            a += week['a'];
+            d += week['d'];
+            c += week['c'];
+        author = {'login': authorName, 'additions':a, 'deletions':d, 'commits':c}
+        print author;
+        authorList.append(author);
+    return jsonify(response);
+
 if __name__ == "__main__":
     app.run()
