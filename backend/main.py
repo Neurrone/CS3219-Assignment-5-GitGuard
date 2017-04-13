ca0832ef559ad18d7f035115e060090843dc6995
Terence Kong <kong_terence@hotmail.com>
2017-04-14 00:03:50 +0800

    implemented sum_contribution sortedby additions/deletions/commits in backend

--- backend/main.py
+++ backend/main.py
@@ -38,6 +38,54 @@ def get_sum_contribution(owner, repo):
         authorList.append(author);
     return jsonify(authorList);
 
+# gets tne summed contributions of each contributor, sorted by additions (for chatbot)
+@app.route('/<owner>/<repo>/sum_contribution/addition', methods=['GET'])
+def get_sum_contribution_sortedby_additions(owner, repo):
+    response = github_api.make_request('repos/{}/{}/stats/contributors'.format(owner, repo))
+    authorList = [];
+
+    for set in response:
+        authorName = set['author']['login'];
+        a = 0;
+        for week in set['weeks']:
+            a += week['a'];
+        author = {'login': authorName, 'additions':a}
+        authorList.append(author);
+        authorList.sort(reverse = True);
+    return jsonify(authorList);
+
+# gets tne summed contributions of each contributor, sorted by deletions (for chatbot)
+@app.route('/<owner>/<repo>/sum_contribution/deletion', methods=['GET'])
+def get_sum_contribution_sortedby_deletions(owner, repo):
+    response = github_api.make_request('repos/{}/{}/stats/contributors'.format(owner, repo))
+    authorList = [];
+
+    for set in response:
+        authorName = set['author']['login'];
+        d = 0;
+        for week in set['weeks']:
+            d += week['d'];
+        author = {'login': authorName, 'deletions':d}
+        authorList.append(author);
+        authorList.sort(reverse = True);
+    return jsonify(authorList);
+
+# gets tne summed contributions of each contributor, sorted by commits (for chatbot)
+@app.route('/<owner>/<repo>/sum_contribution/commit', methods=['GET'])
+def get_sum_contribution_sortedby_commits(owner, repo):
+    response = github_api.make_request('repos/{}/{}/stats/contributors'.format(owner, repo))
+    authorList = [];
+
+    for set in response:
+        authorName = set['author']['login'];
+        c = 0;
+        for week in set['weeks']:
+            c += week['c'];
+        author = {'login': authorName, 'commits':c}
+        authorList.append(author);
+        authorList.sort(reverse = True);
+    return jsonify(authorList);
+
 # returns a list containing the week data (additions, deletions and commits) for a specific user
 @app.route('/<owner>/<repo>/<login>/commit_history', methods=['GET'])
 def get_commit_history(owner, repo, login):
