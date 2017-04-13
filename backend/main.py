49abe0eca72bbbfff10bce56b09fa543ad8d6728
John Yong <whipermr5@gmail.com>
2017-04-13 18:17:07 +0800

    Implement contributions in backend (direct copy from GitHub API stats/contributors)

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,23 @@
 import os, sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
 
-from flask import Flask
+from flask import Flask, jsonify, request, abort
+import flask_cors
+import requests
+
+import github_api as api
+
 app = Flask(__name__)
+flask_cors.CORS(app) # allow cross-origin requests (e.g. from frontend)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def splash():
+    return "GG backend running..."
+
+@app.route('/<owner>/<repo>/contribution', methods=['GET'])
+def get_contribution(owner, repo):
+    response = api.make_request('repos/{}/{}/stats/contributors'.format(owner, repo))
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run()
