5704312e21b40ee6a148bfba4e2b064c53cdf70d
Neurrone <neurrone@users.noreply.github.com>
2017-04-14 16:34:35 +0800

    Bot: implement showing latest commit info

--- backend/bot.py
+++ backend/bot.py
@@ -46,10 +46,34 @@ def top3_contributors(bot, update, args):
         result = 'You must specify the repositorys url. For example, "/top3 https://github.com/TEAMMATES/teammates"'
     bot.sendMessage(chat_id=update.message.chat_id, text=result)
 
+def latest_commit(bot, update, args):
+    result = ''
+    if len(args) > 0:
+        repo_url = args[0]
+        try:
+            owner, repo_name = parse_repo_url(repo_url)
+            latest_commit = github_api.get_latest_commit(owner, repo_name)
+            commit_url = latest_commit['html_url']
+            author_name = latest_commit['commit']['author']['name']
+            author_url = latest_commit['author']['html_url']
+            date_iso_8601 = latest_commit['commit']['author']['date']
+            message = latest_commit['commit']['message']
+            result = 'Latest commit:\n*Author*: [%s](%s)\n*Date*: %s\n*Description*: %s\n*Diff*: %s' \
+                % (author_name, author_url, date_iso_8601, message, commit_url)
+        except ValueError as e:
+            result = '{0}'.format(e)
+    else:
+        result = 'You must specify the repositorys url. For example, "/latest https://github.com/TEAMMATES/teammates"'
+    bot.sendMessage(chat_id=update.message.chat_id, text=result, parse_mode='Markdown', disable_web_page_preview=True)
+
 updater = Updater(token = BOT_TOKEN)
 dispatcher = updater.dispatcher
+
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 top3_handler = CommandHandler('top3', top3_contributors, pass_args=True)
 dispatcher.add_handler(top3_handler)
+latest_handler = CommandHandler('latest', latest_commit, pass_args=True)
+dispatcher.add_handler(latest_handler)
+
 updater.start_polling()
