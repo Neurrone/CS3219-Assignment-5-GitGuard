dde487d29842c33340e265833fcd237f8189b7d0
phadrian <pheh.adrian@gmail.com>
2017-04-14 16:06:19 +0800

    Added filters for modal graph

--- src/components/ContributionModal.js
+++ src/components/ContributionModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Table, Row, Col, Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'elemental';
+import { Row, Col, Modal, ModalHeader, ModalBody, ModalFooter, Button, FormSelect } from 'elemental';
 import Highcharts from 'highcharts';
 import ReactHighcharts from 'react-highcharts'
 import '../../node_modules/elemental/less/elemental.less';
@@ -11,7 +11,19 @@ class ContributionModal extends React.Component {
                 onCancel={this.props.toggleModal} width='large'>
                 <ModalHeader text='Commit History' showCloseButton onClose={this.props.toggleModal} />
                 <ModalBody>
-                    <ReactHighcharts config={this.props.configList[3]}></ReactHighcharts>
+                    <Row>
+                        <Col sm='1/2'>
+                            <FormSelect options={this.props.configList[3].xAxis.categories.map(str => ({value: str, label: str}))}
+                                firstOption={'Select Start Date'} 
+                                onChange={this.props.toggleStartDate} />
+                        </Col>
+                        <Col sm='1/2'>
+                            <FormSelect options={this.props.configList[3].xAxis.categories.map(str => ({value: str, label: str}))} 
+                                firstOption={'Select End Date'} 
+                                onChange={this.props.toggleEndDate} />
+                        </Col>
+                    </Row>
+                    <ReactHighcharts config={this.props.configList[4]}></ReactHighcharts>
                 </ModalBody>
                 <ModalFooter>
                     <Button onClick={this.props.toggleModal}>Close</Button>
